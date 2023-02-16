0c2f4427e95006e4aab96be1e024e585322332fc
Omsri123 <90594988+Omsri123@users.noreply.github.com>
2023-02-16 04:02:29 +0000

    lp

--- mod 1/basiczzz.java
+++ mod 1/basiczzz.java
@@ -1,16 +1,27 @@
-import java.util.Scanner; // import the Scanner class 
+import java.util.*;
+// import java.lang.*;
+// class inp{
+//  public static void main (String args[]){
+//   int a;
+//   a=Integer.parseInt(args[0]);
+//   System.out.println("a is"+a);
+//  }
+// }
 
-class main{
-  public static void main(String[] args){
-    Scanner myObj = new Scanner(System.in);
-    System.out.println("Enter");
-    String username = myObj.nextLine();
-    System.out.println(username);
-  }
-}
+// class main{
+//     public static void main(String[] args){
+//     Scanner myObj = new Scanner(System.in);
+//     System.out.println("Enter");
+//     String username = myObj.nextLine();
+//     System.out.println("Entered value is " + username);
+//   }
+// }
+
 // class pos{
 //   public static void main (String[] args){
-//     int a=0;
+//     Scanner myobj=new Scanner(System.in);
+//     System.out.print("Enter the value : ");
+//     Integer a=myobj.nextInt();
 //     if (a>0)
 //       System.out.println("Poitive");
 //     else if (a==0)
@@ -19,6 +30,7 @@ class main{
 //       System.out.println("Negative");
 //   }
 // }
+
 // class quad{
 //   public static void main(String[] args){
 //     int a=1;
@@ -26,19 +38,20 @@ class main{
 //     int c=3;
 //     double d= Math.sqrt(b*b-4*a*c);
 //     if(d>0){
-//       double x1=(-b+d)/2*a;
-//       double x2=(-b-d)/2*a;
+//       double x1=(-b+d)/(2*a);
+//       double x2=(-b-d)/(2*a);
 //       System.out.println(x1);
 //       System.out.println(x2);
 //     }
 //     else if (d==0){
-//       double x1=(-b+d)/2*a;
+//       double x1=(-b+d)/(2*a);
 //       System.out.println(x1);
 //     }
 //     else
 //       System.out.println("Imajinary roots");
 //   }
 // }
+
 // class gr{
 //   public static void main(String[] args){
 //     int a=16,b=7,c=8;
@@ -50,18 +63,85 @@ class main{
 //       System.out.println("c");
 //   }
 // }
+
+// class week{
+//   public static void main(String[]args){
+//     Scanner myobj = new Scanner(System.in);
+//     System.out.println("Enter number of weekday");
+//     int w=myobj.nextInt();
+//     switch(w){
+//       case 1:
+//         System.out.println("Sunday");
+//         break;
+//       case 2:
+//         System.out.println("Monday");
+//         break;
+//       case 3:
+//         System.out.println("Tuesday");
+//         break;
+//       case 4:
+//         System.out.println("Wedneday");
+//         break;
+//       case 5:
+//         System.out.println("Thursday");
+//         break;
+//       case 6:
+//         System.out.println("Friday");
+//         break;
+//       case 7:
+//         System.out.println("Saturday");
+//         break;
+//       default:
+//         System.out.println("Out of range");
+//     }
+//   }
+// }
+
+// class flt{
+//     public static void main(String[] args){
+//         Scanner Obj = new Scanner(System.in);
+//         System.out.print("Enter a value : ");
+//         float a=Obj.nextFloat();
+//         System.out.print("Enter b value : ");
+//         float b=Obj.nextFloat();
+//         a=a*1000;
+//         b=b*1000;
+//         if (a==b)
+//             System.out.println("Equal");
+//         else
+//             System.out.println("Not Equal");
+//     }
+// }
+
+// class leap{
+//     public static void main(String[] args){
+//         Scanner obj = new Scanner(System.in);
+//         System.out.print("Enter a year : ");
+//         int a=obj.nextInt();
+//         if (a%4==0 && a%4 != 0)
+//             System.out.println("Leap Year");
+//         else
+//             System.out.println("Not Leap Year");
+//     }
+// }
+
 // class nat{
 //   public static void main(String[] args){
 //     for (int i=1;i<=10;i++)
 //       System.out.println(i);
 //   }
 // }
+
 // class mul{
 //   public static void main(String[] args){
+//     Scanner myobj=new Scanner(System.in);
+//     System.out.print("Enter the value : ");
+//     Integer a=myobj.nextInt();
 //     for (int i=1;i<=10;i++)
-//       System.out.println(5*i);
+//       System.out.println(a*i);
 //   }
 // }
+
 // class tri{
 //   public static void main(String[] args){
 //     int n=5;
