3fd2adb61b3adee011c7dd5955762be416cf26b6
Omsri123 <90594988+Omsri123@users.noreply.github.com>
2023-03-06 06:00:19 +0000

    meth

---
+++ mod 2/method.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class method {
+    void compare(int x , int y ){
+        if (x > y) System.out.println("a is greater");
+        else System.out.println("b is greater");
+    }
+    void compare(char x , char y ){
+        if (x > y) System.out.println("a is greater");
+        else System.out.println("b is greater");
+    }
+}
+class main{
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        method com = new method();
+        int c;
+        System.out.println("Enter 1 for int comparision\nEnter 2 for char comparision\nEnter 3 for string comparision");
+        c= obj.nextInt();
+        switch ( c ) {
+            case 1:
+                int a,b ;
+                System.out.println("Enter a");
+                a= obj.nextInt();
+                System.out.println("Enter b");
+                b = obj.nextInt();
+                com.compare( a , b );
+                break;
+            case 2:
+                char a,b ;
+                System.out.println("Enter a");
+                a= obj.next()
+                System.out.println("Enter b");
+                b = obj.next();
+                com.compare( a , b );
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
--- mod 2/progzzz.java
+++ mod 2/progzzz.java
@@ -52,3 +52,36 @@
 //     }
 // }
 
+// import java.util.*;
+// class arth{
+//     int sum(int x , int y){
+//         return (x+y);
+//     }
+//     int sub(int x , int y){
+//         return (x-y);
+//     }
+//     int mul(int x , int y){
+//         return (x*y);
+//     }
+//     int div(int x , int y){
+//         return (x/y);
+//     }
+//     int rem(int x , int y){
+//         return (x%y);
+//     }
+// public static void main(String[] args){
+//     Scanner obj = new Scanner(System.in);
+//     int a , b;
+//     System.out.print("Enter a: ");
+//     a = obj.nextInt();
+//     System.out.print("Enter b: ");
+//     b = obj.nextInt();
+//     arth n = new arth();
+//     System.out.println(a + " + " + b + " = " + n.sum(a, b));
+//     System.out.println(a + " - " + b + " = " + n.sub(a, b));
+//     System.out.println(a + " x " + b + " = " + n.mul(a, b));
+//     System.out.println(a + " / " + b + " = " + n.div(a, b));
+//     System.out.println(a + " % " + b + " = " + n.rem(a, b));
+//     }
+// }
+
